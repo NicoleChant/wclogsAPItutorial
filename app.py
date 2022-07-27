ae3fe791f8f3987f8d3d5a646e6f228dd5e2a590
Nicole Chantzi <nicolechantzi@gmail.com>
2022-07-27 16:01:03 +0300

    added type hints

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import requests
 import os
 import json
 from functools import reduce
+from typing import Union
 
 apiURL = "https://www.warcraftlogs.com/api/v2/client"
 tokenURL = "https://www.warcraftlogs.com/oauth/token"
@@ -9,17 +10,20 @@ tokenURL = "https://www.warcraftlogs.com/oauth/token"
 def get_token(store : bool = False) -> requests.models.Response:
     """Retrieves access token for a given client id and secret."""
     data = {"grant_type": "client_credentials"}
-    auth = (os.environ.get("user_id"),
-                os.environ.get("user_secret"))
+
+    ##we have exported our variables to our shell environment
+    auth = (os.environ.get("user_id"), os.environ.get("user_secret"))
     with requests.Session() as session:
         response = session.post(tokenURL ,
                                                 data = data ,
                                                 auth = auth)
+
+    ##stores the token if the the request was authorized
     if store and response.status_code == 200:
         store_token(response)
     return response
 
-def store_token(response : requests.models.Response):
+def store_token(response : requests.models.Response) -> None:
     """Stores token to a hidden json file."""
     try:
         with open(".credentials.json" , mode = "w+" , encoding = "utf-8") as file:
@@ -29,18 +33,20 @@ def store_token(response : requests.models.Response):
         print("Could not create the file!")
 
 
-def retrieve_token():
+def retrieve_token() -> Union[None , dict[str , str]]:
     """Extracts access token from json file."""
     try:
         with open(".credentials.json" , mode = "r+" , encoding = "utf-8") as file:
             return json.load(file)
     except OSError as e:
         print(e)
-        return
+        return None
 
 def retrieve_headers() -> dict[str , str]:
     """Retrieves authorization headers to use the public wclogs API."""
     return {"Authorization":f"Bearer {retrieve_token().get('access_token')}"}
+
+##Specify a query
 
 reportData = """query ($code:String){
         reportData{
@@ -55,7 +61,7 @@ reportData = """query ($code:String){
                     }
                     }"""
 
-def getData(query , **kwargs):
+def getData(query : str , **kwargs) -> Union[list[dict[str , Union[str , int]]] , str]:
     """Sends a general query to wclogs API to retrieve report data. The kwargs contain information about the particular endpoint.
     we wish to extract data from. For instance, if we want to extract reportData we must specify as keyword argument the code
     for the report. In general, we the kwargs must contain all the arguments of the query otherwise an exception will be raised.
